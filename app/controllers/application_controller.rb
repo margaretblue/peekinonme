0ad934016b1378c9b5805885486d83cd9b8e356a
John Johnson <dampboxdev@gmail.com>
2014-02-04 15:19:55 -0800

    params added to devise user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
 
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << [:patient_email, :caregiver_email, :patient_name, :patient_street, :patient_city, :patient_zip, :patient_condition, :patient_phone, :caregiver_name, :caregiver_phone, :patient_phone]
+    end
 end
--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -1,6 +1,6 @@
 class User::ParameterSanitizer < Devise::ParameterSanitizer
-  private
-  def account_update
-    default_params.permit(:name, :phone, :street, :city, :zip, :condition,:checkintime)
-  end
+    private
+    def account_update
+        default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :caregiver_name, :patient_name, :caregiver_phone, :patient_phone, :patient_email, :patient_street, :patient_city, :patient_zip, :checkintime, :patient_condition, :patient_token)
+    end
 end
