94d68f903695ceed4ac38779c784ac47b5bb9700
Margaret Morris <margaretblue@gmail.com>
2014-02-05 10:42:48 -0800

    css styling, forms, remove scaff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Peekinonme::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   #devise
-  config.action_mailer.default_url_options = { :host => 'peekinonme.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'checkinonme.herokuapp.com' }
 end
