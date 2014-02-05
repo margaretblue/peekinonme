c19aa7f5cb63162646a0f23e39ef9e6e8e57e0aa
John Johnson <dampboxdev@gmail.com>
2014-02-05 14:17:08 -0800

    patients routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Peekinonme::Application.routes.draw do
-  resources :checkins
+
 
   root 'welcome#index'
 
   devise_for :users, :controllers => { :registrations => 'registrations'}, :path => 'account'
 
-  resources :users, :only => [:show] do
+  resources :users, shallow: true do
     resources :patients
+    resources :checkins
+
   end
 
 end
