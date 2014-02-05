893c12696942de6bbd369e08a747a1ea90fc4ec5
Margaret Morris <margaretblue@gmail.com>
2014-02-05 12:15:41 -0800

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140205061034) do
+ActiveRecord::Schema.define(version: 20140205192833) do
+
+  create_table "checkins", force: true do |t|
+    t.integer  "patient_id"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "patients", force: true do |t|
     t.string   "name"
