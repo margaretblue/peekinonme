d1226000f9fe1a26d6d1703e7bcbce397b42e885
kaxla <kaylas.email.address@gmail.com>
2014-02-06 14:49:05 -0800

    fucking commas pt. 2

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,14 +2,14 @@ class UserMailer < ActionMailer::Base
   default from: "hireme@kaylaarianamorrison.com"
 
   def welcome_email (user)
-    @user = user
-    @url = http://checkinonme.herokuapp.com/account/sign_in
+    @user = user,
+    @url = http://checkinonme.herokuapp.com/account/sign_in,
     mail(to: @user.email, subject: 'Welcome to Check In On Me')
   end
 
   def welcome_patient (patient)
-    @patient = patient
-    token = @patient.token
+    @patient = patient,
+    token = @patient.token,
     @url = "http:/checkinonme.herokuapp.com/patients/#{token}/checkins/new"
     mail(to: @patient.email, subject: 'Welcome to Your Check In On Me Portal')
   end
