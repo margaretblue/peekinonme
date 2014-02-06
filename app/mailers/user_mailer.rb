71269856d4ee9a224b74ac04d1dfb8101cd98cf8
kaxla <kaylas.email.address@gmail.com>
2014-02-06 13:47:51 -0800

    add postmark

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,24 @@
 class UserMailer < ActionMailer::Base
-  default from: "margaretblue@gmail.com"
+  default from: "hireme@kaylaarianamorrison.com"
 
   def welcome_email(user)
-    @user = user
-    @url  = 'http://checkinonme.herokuapp.com/account/sign_in'
-    mail(to: @user.email, subject: 'Welcome to Check In On Me')
+    mail(
+      :subject => 'Welcome to Check In On Me'
+      :to => @user.email
+    )
+    # @user = user
+    # @url  = 'http://checkinonme.herokuapp.com/account/sign_in'
+    # mail(to: @user.email, subject: 'Welcome to Check In On Me')
   end
 
   def welcome_patient(patient)
-    @patient = patient
-    token = @patient.token
-    @url = "http:/checkinonme.herokuapp.com/patient?token=#{token}"
-    mail(to: @patient.email, subject: 'Welcome to Your Check In On Me Portal')
+    mail(
+      :subject => 'Welcome to Your Check In On Me'
+      :to => @patient.email
+    )
+    # @patient = patient
+    # token = @patient.token
+    # @url = "http:/checkinonme.herokuapp.com/patient?token=#{token}"
+    # mail(to: @patient.email, subject: 'Welcome to Your Check In On Me Portal')
   end
 end
