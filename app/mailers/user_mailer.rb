ede39636c55d5384e14bf2c2d74f53c5b5ed3513
kaxla <kaylas.email.address@gmail.com>
2014-02-06 14:46:20 -0800

    email fixed?

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,29 +1,16 @@
 class UserMailer < ActionMailer::Base
   default from: "hireme@kaylaarianamorrison.com"
 
-  def welcome_email
-
-    client.deliver(to: @user.email,
-                   subject: 'subject')
-    # mail(
-    #   :subject => 'Welcome to Check In On Me'
-    #   :to => @user.email
-    # )
-
-
-    # @user = user
-    # @url  = 'http://checkinonme.herokuapp.com/account/sign_in'
-    # mail(to: @user.email, subject: 'Welcome to Check In On Me')
+  def welcome_email (user)
+    @user = user
+    @url = http://checkinonme.herokuapp.com/account/sign_in
+    mail(to: @user.email, subject: 'Welcome to Check In On Me')
   end
 
-  def welcome_patient
-    # mail(
-    #   :subject => 'Welcome to Your Check In On Me'
-    #   :to => @patient.email
-    # )
-    # @patient = patient
-    # token = @patient.token
-    # @url = "http:/checkinonme.herokuapp.com/patient?token=#{token}"
-    # mail(to: @patient.email, subject: 'Welcome to Your Check In On Me Portal')
+  def welcome_patient (patient)
+    @patient = patient
+    token = @patient.token
+    @url = "http:/checkinonme.herokuapp.com/patients/#{token}/checkins/new"
+    mail(to: @patient.email, subject: 'Welcome to Your Check In On Me Portal')
   end
 end
